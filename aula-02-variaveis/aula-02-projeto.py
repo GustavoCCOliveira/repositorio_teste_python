c912bc0ccdda31be451167f53c146127a2a0e085
GustavoCCOliveira <gustavotorresdeoliveira20@gmail.com>
2026-03-17 11:41:09 -0300

    Aula 02

---
+++ aula-02-variaveis/aula-02-projeto.py
@@ -0,0 +1,39 @@
+# print("hello world")
+#
+# print(7 * 2)
+# print("7" + "1") #concatenação de strings
+#
+# #coments for a one line
+#
+#
+# #variaveis
+#
+# # Nome = "alexandre"
+# # Peso = 76.5
+# # Idade = 25
+# #
+# # print(Nome, idade, peso)
+# # print(f"Olaa, {Nome}!")
+#
+# #input simula um formulario no CMD
+#
+# nome = input("digite seu nome:")
+# idade = int(input"sua idade:")
+# peso = int(input"Digite seu peso:")
+#
+# print(nome, idade, peso)
+#
+
+# Nome_User = input("Qual o seu nome:")
+# print(f"boas vindas {Nome_User}")
+#
+
+# Dia = input("que dia é hoje? :")
+# Mes = input("que mes é:")
+# Ano = input("em que ano estamos:")
+# print(f"Hoje é dia {Dia} do Mes {Mes} e estamos no ano de {Ano}")
+
+# N1 = int(input("informe o numero"))
+# N2= int(input("informe o numero 2:"))
+# N3= N1 + N2
+# print(f"o resultado de {N1} + {N2} é {N3}")
